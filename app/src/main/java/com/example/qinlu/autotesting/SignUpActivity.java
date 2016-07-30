33de46359355b3517215f990fa289ac7baca271a
mixlu <6qinlu6@gmail.com>
2016-07-29 21:38:09 -0700

    initial commit

---
+++ app/src/main/java/com/example/qinlu/autotesting/SignUpActivity.java
@@ -0,0 +1,64 @@
+package com.example.qinlu.autotesting;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by qinlu on 7/28/2016.
+ */
+public class SignUpActivity extends Activity{
+    EditText editTextUserName,editTextPassword,editTextConfirmPassword;
+    Button btnCreateAccount;
+
+    LoginDataBaseAdapter loginDataBaseAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        // get Instance  of Database Adapter
+        loginDataBaseAdapter = new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter = loginDataBaseAdapter.open();
+
+        // Get References of Views
+        editTextUserName = (EditText)findViewById(R.id.editTextUserName);
+        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = (EditText)findViewById(R.id.editTextConfirmPassword);
+
+        btnCreateAccount = (Button)findViewById(R.id.buttonCreateAccount);
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String userName = editTextUserName.getText().toString();
+                String password = editTextPassword.getText().toString();
+                String confirmPassword = editTextConfirmPassword.getText().toString();
+                // check if any of the fields are empty
+                if(userName.equals("") || password.equals("") || confirmPassword.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Please fill in all inputs", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // check if user already exist
+                if(loginDataBaseAdapter.isUserExist(userName)) {
+                    // TODO: Adding account updating logic or password retrieve logic
+                    Toast.makeText(getApplicationContext(), "Account already existed", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // check if both password matches
+                if(!password.equals(confirmPassword)) {
+                    Toast.makeText(getApplicationContext(), "Password does not match", Toast.LENGTH_LONG).show();
+                    return;
+                } else {
+                    // Save the Data in Database
+                    loginDataBaseAdapter.insertEntry(userName, password);
+                    Toast.makeText(getApplicationContext(), "Account Successfully Created ", Toast.LENGTH_LONG).show();
+                    // Redirect to login page after register
+                    Intent intentLogin = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intentLogin);
+                }
+            }
+        });
+    }
+}
