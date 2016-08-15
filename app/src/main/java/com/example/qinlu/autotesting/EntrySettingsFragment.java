2ce424423d7d64219144ed1d30b805bae99c6852
Qin <qinlu@amazon.com>
2016-08-14 17:01:18 -0700

    Added settings page toolbar

--- app/src/main/java/com/example/qinlu/autotesting/EntrySettingsFragment.java
+++ app/src/main/java/com/example/qinlu/autotesting/EntrySettingsFragment.java
@@ -1,7 +1,18 @@
 package com.example.qinlu.autotesting;
 
+import java.util.ArrayList;
+
+import android.app.ListActivity;
+import android.util.SparseBooleanArray;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.preference.PreferenceFragment;;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by qinlu on 8/10/16.
@@ -20,16 +31,69 @@ public class EntrySettingsFragment extends PreferenceFragment {
      * 废气检验员管理 Fragment
      */
     public static class EmissionInspectorFragmentInner extends PreferenceFragment {
-//        @Override
-//        public void onCreate(Bundle savedInstanceState) {
-//            super.onCreate(savedInstanceState);
+        @Override
+        public final View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+                                       final Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.test_fragment, container, false);
+
+            /** Items entered by the user is stored in this ArrayList variable */
+            ArrayList list = new ArrayList();
+
+            /** Declaring an ArrayAdapter to set items to ListView */
+//            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, list);
+            ;
+            /** Reference to the add button of the layout main.xml */
+            Button btn = (Button)v.findViewById(R.id.btnAdd);
+
+            /** Reference to the delete button of the layout main.xml */
+            Button btnDel = (Button)v.findViewById(R.id.btnDel);
+
+            /** Defining the ArrayAdapter to set items to ListView */
+//            adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, list);
+
+            /** Defining a click event listener for the button "Add" */
+//            OnClickListener listener = new OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    EditText edit = (EditText)v.findViewById(R.id.txtItem);
+//                    list.add(edit.getText().toString());
+//                    edit.setText("");
+//                    adapter.notifyDataSetChanged();
+//                }
+//            };
+
+            /** Defining a click event listener for the button "Delete" */
+//            OnClickListener listenerDel = new OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    /** Getting the checked items from the listview */
+//                    SparseBooleanArray checkedItemPositions = getListView().getCheckedItemPositions();
+//                    int itemCount = getListView().getCount();
 //
-//            // Can retrieve arguments from preference XML.
-//            Log.i("args", "Arguments: " + getArguments());
+//                    for(int i=itemCount-1; i >= 0; i--){
+//                        if(checkedItemPositions.get(i)){
+//                            adapter.remove(list.get(i));
+//                        }
+//                    }
+//                    checkedItemPositions.clear();
+//                    adapter.notifyDataSetChanged();
+//                }
+//            };
+
+            /** Setting the event listener for the add button */
+//            btn.setOnClickListener(listener);
 //
-//            // Load the preferences from an XML resource
-////            addPreferencesFromResource(R.xml.fragmented_preferences_inner);
+//            /** Setting the event listener for the delete button */
+//            btnDel.setOnClickListener(listenerDel);
+//
+//            /** Setting the adapter to the ListView */
+//            setListAdapter(adapter);
+            return v;
+        }
     }
+    /**
+     * 外检员管理 Fragment
+     */
     public static class ExteriorInspectorFragmentInner extends PreferenceFragment {
 //        @Override
 //        public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +105,9 @@ public class EntrySettingsFragment extends PreferenceFragment {
 //            // Load the preferences from an XML resource
 ////            addPreferencesFromResource(R.xml.fragmented_preferences_inner);
     }
+    /**
+     * 引车员管理 Fragment
+     */
     public static class CommandInspectorFragmentInner extends PreferenceFragment {
 //        @Override
 //        public void onCreate(Bundle savedInstanceState) {
@@ -52,13 +119,41 @@ public class EntrySettingsFragment extends PreferenceFragment {
 //            // Load the preferences from an XML resource
 ////            addPreferencesFromResource(R.xml.fragmented_preferences_inner);
     }
+    /**
+     * 厂牌型号 Fragment
+     */
     public static class MakeModelFragmentInner extends PreferenceFragment {
 
     }
+    /**
+     * 车牌号码前缀 Fragment
+     */
     public static class PlatePrefixFragmentInner extends PreferenceFragment {
 
     }
+    /**
+     * 检测单位 Fragment
+     */
     public static class OrgFragmentInner extends PreferenceFragment {
+
+    }
+    /**
+     * 设备选择 Fragment
+     */
+    public static class EquipmentFragmentInner extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.equipment_management);
+        }
+
+    }
+    /**
+     * 打印报告 Fragment
+     */
+    public static class PrintFragmentInner extends PreferenceFragment {
 
     }
 }
--- app/src/main/java/com/example/qinlu/autotesting/SettingsActivity.java
+++ app/src/main/java/com/example/qinlu/autotesting/SettingsActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBar;
 
 import java.util.List;
 
@@ -14,17 +17,16 @@ import java.util.List;
  */
 
 // TODO: add toolbar
-public class SettingsActivity extends PreferenceActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (hasHeaders()) {
-            Button button = new Button(this);
-            button.setText("Exit");
-            setListFooter(button);
-        }
-    }
+public class SettingsActivity extends AppCompatPreferenceActivity {
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (hasHeaders()) {
+//            Button button = new Button(this);
+//            button.setText("Exit");
+//            setListFooter(button);
+//        }
+//    }
 
     @Override
     protected boolean isValidFragment(String fragmentName) {
@@ -35,5 +37,26 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.settings_nav, target);
+
+        setContentView(R.layout.page_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar bar = getSupportActionBar();
+        bar.setHomeButtonEnabled(true);
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setDisplayShowTitleEnabled(true);
+//        bar.setHomeAsUpIndicator(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        bar.setTitle("设置");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
